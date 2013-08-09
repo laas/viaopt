d230ee3bd686a5016b7914ec7a7c743541192f8e
Nicolas Mansard <nmansard@laas.fr>
2013-08-09 15:22:18 -0700

    IVIGIT unitest tfunction.

---
+++ unitTesting/tfunctions.cpp
@@ -0,0 +1,35 @@
+ /* -------------------------------------------------------------------------- *
+ * 
+ * Unittest of the Model classes.
+ * 
+ * -------------------------------------------------------------------------- */
+
+
+#include "viaopt/model-awas.hpp"
+#include "viaopt/ilqr.hpp"
+#include <iostream>
+
+using namespace viaopt;
+int main (int , char** )
+{
+ 
+  Ilqr<ModelAwas> ilqr;
+  ilqr.init();
+  ModelAwas::State_t state = ModelAwas::initState(0.0,0.1);
+  assert(state.NbCols()==1);
+  ModelAwas::Control_t control(2);
+  control(0) = 0.15;
+  control(1) = 0.4;
+
+  double dT = 0.001;
+  double inertia = 0.27;
+  double stiffness = 10000.0;
+  double Wx = 0.1;
+  double Wlim = 0.01;
+  double Tdes = 50;
+  double Wu0 = 0.1;
+  double Wu1 = 0.001;
+  
+
+
+}
